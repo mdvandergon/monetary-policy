3569988e41ac097b9f4a673b25f23273a1f87cc8
Mark Vandergon <mdvandergon@gmail.com>
2017-11-28 22:34:51 -0600

    WIP: differencing, recessions, tooltips and update
    - differencing plots but is not visible
    - recessions plots but the bars do not line up with actual dates
    - tool tips renders but it doesn’t get data — could be difficult
    - update sort of works

--- graph.js
+++ graph.js
@@ -4,7 +4,7 @@ The animated monetary policy charts
 */
 
 // width and height are set by the render
-var margin = {top: 20, right: 0, bottom: 40, left: 50};
+var margin = {top: 20, right: 40, bottom: 40, left: 50};
 
 function drawLine(path, duration=2500) {
   var totalLength = path.node().getTotalLength();
@@ -18,14 +18,14 @@ function drawLine(path, duration=2500) {
 }
 
 function makeLegend() {
-  var legendSize = 100;
+  var legendSize = 200;
   var ordinal = d3.scaleOrdinal()
     .domain(["Fed Funds Rate", "Taylor Rule"])
     .range([ "#666666", "#2F74FF"]);
 
   var legend = d3.select('svg').append("g")
     .attr("class", "legend")
-    .attr("transform", "translate("+(width - 2*legendSize - margin.right) +","+ 2*margin.top +")");
+    .attr("transform", "translate("+(width - legendSize) +","+ 2*margin.top +")");
 
   var legendOrdinal = d3.legendColor()
     .shape("path", d3.symbol().type(d3.symbolCircle).size(legendSize)())
@@ -40,11 +40,12 @@ function makeLegend() {
 
   var svg = d3.select('#graph').append('svg')
     .attrs({
-      width: width,
-      height: height
+      width: svgWidth,
+      height: svgHeight
     })
-  width = +svg.attr("width") - margin.left - margin.right
-  height =  +svg.attr("height") - margin.top - margin.bottom
+  var width = +svg.attr("width") - margin.left - margin.right;
+  var height =  +svg.attr("height") - margin.top - margin.bottom;
+
   var g = svg.append('g')
     .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
 
@@ -65,10 +66,13 @@ function makeLegend() {
     d.DATE = parseTime(d.DATE);
     d.FEDFUNDS = +d.FEDFUNDS;
     d.TAYLORRATE = +d.TAYLORRATE;
+    d.GDPDEF_PC1 = +d.GDPDEF_PC1;
+    d.CPILFESL_PC1 = +d.CPILFESL_PC1;
+    d.GDPPOT = +d.GDPPOT;
+    d.OUTPUTGAP = (d.GDPDEF_PC1- d.CPILFESL_PC1)/d.GDPPOT;
     return d;
   }, function(error, data) {
     if (error) throw error;
-    data = data
     x.domain(d3.extent(data, function(d) { return d.DATE; }));
     y.domain(d3.extent(data, function(d) { return d.FEDFUNDS; }));
 
@@ -81,13 +85,8 @@ function makeLegend() {
       .y(function(d) { return y(d.TAYLORRATE); })
       .curve(d3.curveCatmullRom.alpha(0.5));
 
-      var taylor_rate = d3.line()
-        .x(function(d) { return x(d.DATE); })
-        .y(function(d) { return y(d.TAYLORRATE); })
-        .curve(d3.curveCatmullRom.alpha(0.5));
-
     g.append("g")
-      .attr("transform", "translate(0," + height + ")")
+      .attr("transform", "translate(0," + height+ ")")
       .call(d3.axisBottom(x))
       .attr('class', 'axis')
       .select(".domain")
@@ -104,6 +103,21 @@ function makeLegend() {
       .attr("text-anchor", "end")
       .text("Interest Rate (%)");
 
+    // Initialize tooltip
+    var tip = d3.tip()
+      .attr('class', 'd3-tip')
+      .offset([100, 0])
+      .html(function(d) {
+        console.log(d.OUTPUTGAP, d.GDPDEF_PC1)
+        return    "<h6>Taylor Rate Calculation</h6>"
+                  +"<p>growth: " + (d.GDPDEF_PC1 + 0.02) + "<br>"
+                  + "inflation: "+ 2 +"%<br>"
+                  + "output gap: "+ d.OUTPUTGAP + "%<br>"
+                  + "<h6>=<span style='color:#E82C0C'> " + d.TAYLORRATE + "</span></h6>";
+      })
+    g.call(tip)
+
+    // plotting the fed funds rate
     var ff = g.append("path")
       .attr("id", "fed-funds")
       .datum(data)
@@ -114,25 +128,53 @@ function makeLegend() {
       .attr("stroke-width", 2)
       .attr("d", fed_funds);
 
-      var tr =  g.append("path")
-        .style("opacity", 0)
-        .attr("id", "taylor-rate")
-        .datum(data)
-        .attr("fill", "none")
-        .attr("stroke", "#2F74FF")
-        .attr("stroke-linejoin", "round")
-        .attr("stroke-linecap", "round")
-        .attr("stroke-width", 3)
-        .attr("d", taylor_rate);
+    // plotting the taylor rate
+    var tr =  g.append("path")
+      .style("opacity", 0)
+      .attr("id", "taylor-rate")
+      .datum(data)
+      .attr("fill", "none")
+      .attr("stroke", "#2F74FF")
+      .attr("stroke-linejoin", "round")
+      .attr("stroke-linecap", "round")
+      .attr("stroke-width", 3)
+      .attr("d", taylor_rate)
+      .on('mouseover', tip.show)
+      .on('mouseout', tip.hide);
 
+    // create shading for the difference between rates
+    var trDiff = d3.area()
+      .x(function(d) { return x(d.DATE); })
+      .y(function(d) {return y(Math.max(d.FEDFUNDS, d.TAYLORRATE))})
+      .y0(function(d) {return y(d.TAYLORRATE)});
+
+    var ffDiff = d3.area()
+      .x(function(d) { return x(d.DATE); })
+      .y(function(d) {return y(Math.max(d.FEDFUNDS, d.TAYLORRATE))})
+      .y0(function(d) {return y(d.FEDFUNDS)});
+
+    g.append("path")
+      .style("fill", "green")
+      .style("fill-opacity", .5)
+      .attr("class", "difference")
+      .attr("d", trDiff(data))
+
+    g.append("path")
+      .style("fill", "red")
+      .style("fill-opacity", .5)
+      .attr("class", "difference")
+      .attr("d", ffDiff(data))
+
+    // only draw the fed funds rate. draw taylor on scroll
     drawLine(ff);
-
   });
 
   // Adding the Taylor Rate
   function addTaylor() {
-
+    // increase the contrast b/t lines
     var ff = d3.select('#fed-funds')
+                .transition()
+                .delay(250)
                 .attr("stroke-width", 1)
                 .attr("stroke", "#888888")
 
@@ -141,33 +183,14 @@ function makeLegend() {
     makeLegend();
   }
 
-  function addCalc() {
-    console.log(g)
-    g.append('div')
-      .attr('id', 'rate-calculator')
-      .html("<p class=text-center> % change in gdp \
-                  + inflation \
-                  + 0.5*(% in real gdp) \
-                  + 0.5*(output gap) + </p>")
+  function addDiff() {
+    // https://bl.ocks.org/mbostock/3894205
 
-    /* Initialize tooltip */
-    var tip = d3.tip()
-      .attr('class', 'd3-tip')
-      .offset([-10, 0])
-      .html(function(d) {
-        return "<strong>Taylor:</strong> <span style='color:red'>" + d.TAYLORRATE + "</span>";
-      })
-    g.call(tip)
-
-    g.selectAll('line')
-      .on('mouseover', tip.show)
-      .on('mouseout', tip.hide)
+    var tr = d3.select('#taylor-rate').node()
+    var ff = d3.select('#fed-funds').node()
   }
 
   function addRecessions() {
-
-    // var x_bar = d3.scale.ordinal()
-    // .rangeRoundBands([0, width], padding=0.1);
 
     d3.csv('data/recessions.csv', function(d) {
       // wrangle
@@ -175,28 +198,31 @@ function makeLegend() {
       d.TROUGH = parseTime(d.TROUGH);
       return d;
     }, function(error, recessions) {
+
+      x.domain(d3.extent(recessions, function(d) { return d.PEAK; }));
       g.selectAll('bar')
         .data(recessions)
         .enter()
         .append('rect')
-        .attr('fill', '#f0f0f0')
-        .attr('opacity', '0.1')
+        .attr('class', 'recession-bar')
+        .transition()
+        .delay(250)
         .attr('x', function(d) { return x(d.PEAK); })
         .attr('y', 1)
-        .attr('width', function(d) { return x(d.TROUGH)-x(d.PEAK); }) //xScale.bandwidth())
+        .attr('width', function(d) { return x(d.TROUGH) - x(d.PEAK); })
         .attr('height', height)
         .attr('rx', 2)
         .attr('ry', 2)
         .style('fill', function(d) {
             var gr
-            if (d.recession == "12/1/07") { gr = "#FF6548"}
+            if (d.PEAK == "12/1/07") { gr = "#FF6548"}
             return gr
           });
       });
     };
 
   function addInflation() {
-
+      // we'll add a modified taylor rule
   }
 
   // update functions
@@ -205,7 +231,7 @@ function makeLegend() {
 
   // update the graph at these positions
   updateFunctions[2] = addTaylor;
-  updateFunctions[3] = addCalc;
+  updateFunctions[3] = addDiff;
   updateFunctions[4] = addRecessions;
   updateFunctions[5] = addInflation;
 
@@ -215,13 +241,18 @@ function makeLegend() {
     .graph(d3.selectAll('#graph'))
     .container(d3.select('#container-1'))
     .sections(d3.selectAll('#container-1 section > div'))
-    // .eventId('c1')
+    .eventId('c1')
     .on('active', function(i){
       activeI = i
       console.log("Update Function to call:",i);
 
       //call all fns last and active index
       var sign = activeI - lastI < 0 ? -1 : 1
+      if (sign < 1) {
+        // clear graph - not perfect
+        d3.select("svg").remove()
+        makePlot();
+      }
       d3.range(lastI + sign, activeI + sign, sign).forEach(function(i){
         updateFunctions[i]()
       })
